7bbc646c16a7629a341f77cb3e480a50478d9d12
Alexis Davidson <alexisdavidson00@gmail.com>
2022-10-28 09:29:57 +0800

    prep ally

--- src/frontend/components/Ally.js
+++ src/frontend/components/Ally.js
@@ -1,21 +1,26 @@
 
 import { Image, Row, Col, Button } from 'react-bootstrap'
-import skull from './assets/FREAKYsample.png'
+import skull from './assets/ExampleRABBIT 1.png'
+import a1 from './assets/a1.png'
 
 const Ally = () => {
     return (
-        <Row className="topBg">
-            <Row className="top pe-0">
-                <Row>
-                    <Col className="my-auto col-12 col-lg-6 text-light">
+        <Row className="">
+            <Row className="top m-auto pe-0">
+                <Row className="m-auto" style={{width: "auto"}}>
+                    <Col className="m-auto col-12 col-lg-6 text-light">
+                        <Row className="m-auto" style={{width: "auto"}}>
+                            <img className="m-auto" src={a1} height="100px" width="100px" />
+                        </Row>
                         <Row className="pt-3 bigText">
-                            <p className="p-0" style={{textAlign: "left"}}>
-                                <span className="pinkTextColor">UNLOCK</span> the magic of art. Join WE and become yourself 2.0
+                            <p className="p-0" style={{textAlign: "left", fontWeight: "normal"}}>
+                                <span className="blueTextColor">ALLY COLLECTIONS</span> can be minted for free as long as you have we coins, which you can earn with your reference link.
                             </p>
                         </Row>
-                        <Row>
-                            <p className="p-0" style={{textAlign: "left"}}>
-                                WE&ME is an art project, related to streetwear and unexpected airdrops.
+                        <Row className="pt-3">
+                            <p className="p-0" style={{textAlign: "left", fontWeight: "normal"}}>
+                                *10 000 every season, generative<br/>
+                                <span style={{fontWeight: "bold", fontStyle: "italic"}}>COMING SOON!</span>
                             </p>
                         </Row>
                         <Row>
