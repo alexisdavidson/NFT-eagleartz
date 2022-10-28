8feaf7a9417990c40e6372b2e925a173171ed58b
Alexis Davidson <alexisdavidson00@gmail.com>
2022-10-28 12:53:09 +0800

    finalize ally

--- src/frontend/components/Ally.js
+++ src/frontend/components/Ally.js
@@ -6,11 +6,13 @@ import a1 from './assets/a1.png'
 const Ally = () => {
     return (
         <Row className="">
-            <Row className="top m-auto pe-0">
+            <Row className="top m-auto">
                 <Row className="m-auto" style={{width: "auto"}}>
                     <Col className="m-auto col-12 col-lg-6 text-light">
                         <Row className="m-auto" style={{width: "auto"}}>
-                            <img className="m-auto" src={a1} height="100px" width="100px" />
+                            <Col className="m-auto" style={{textAlign: "left"}}>
+                                <Image src={a1} className="allyimage" />
+                            </Col>
                         </Row>
                         <Row className="pt-3 bigText">
                             <p className="p-0" style={{textAlign: "left", fontWeight: "normal"}}>
